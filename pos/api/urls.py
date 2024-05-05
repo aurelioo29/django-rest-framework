bd8e9a125486f620c47ee31d4efb40b35f962bc3
aurelioo29 <aureliolucio5@gmail.com>
2024-05-05 21:31:08 +0700

    add Method Put & Delete

--- pos/api/urls.py
+++ pos/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from api import views
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import (TableRestoListApiView)
+from .views import (TableRestoListApiView, TableRestoDetailApiView)
 
 app_name = "api"
 
@@ -9,5 +9,6 @@ urlpatterns = [
   # path('api/v1/login', LoginView.as_view()),
   # path('api/v1/logout', LogoutView.as_view()),
   # path('api/v1/register', RegisterWaitressAPI.as_view()),
-  path('api/table_resto', views.TableRestoListApiView.as_view())
+  path('api/table_resto', views.TableRestoListApiView.as_view()),
+  path('api/table_resto/<int:id>', views.TableRestoDetailApiView.as_view()),
 ]
--- pos/api/views.py
+++ pos/api/views.py
@@ -6,12 +6,13 @@ from pos_app.models import TableResto
 from api.serializers import TableRestoSerializer
 
 class TableRestoListApiView(APIView):
-
+  # method get
   def get(self, request, *args, **kwargs):
     table_restos = TableResto.objects.all()
     serializer = TableRestoSerializer(table_restos, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
   
+  # method post
   def post(self, request, *args, **kwargs):
     data = {
       'code': request.data.get('code'),
@@ -28,3 +29,80 @@ class TableRestoListApiView(APIView):
       }
       return Response(response, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+class TableRestoDetailApiView(APIView):
+  # method get specific data
+  def get_object(self, id):
+    try:
+      return TableResto.objects.get(id=id)
+    except TableResto.DoesNotExist:
+      return None
+  
+  def get(self, request, *args, **kwargs):
+    table_resto_instance = self.get_object(id)
+    if not table_resto_instance:
+      return Response(
+        {
+          'status': status.HTTP_400_BAD_REQUEST,
+          'message': 'Data not found',
+          'data': {}
+        }, status=status.HTTP_400_BAD_REQUEST
+      )
+    
+    seriliazer = TableRestoSerializer(table_resto_instance)
+    response = {
+      'status': status.HTTP_400_BAD_REQUEST,
+      'message': 'Data retrieved successfully',
+      'data': seriliazer.data
+    }
+    return Response(response, status=status.HTTP_200_OK)
+  
+  # method PUT
+  def put(self, request, *args, **kwargs):
+    table_resto_instance = self.get_object(id)
+    if not table_resto_instance:
+      return Response(
+        {
+          'status': status.HTTP_400_BAD_REQUEST,
+          'message': 'Data not found',
+          'data': {}
+        }, status=status.HTTP_400_BAD_REQUEST
+      )
+    
+    data = {
+      'code': request.data.get('code'),
+      'name': request.data.get('name'),
+      'capacity': request.data.get('capacity'),
+      'table_status': request.data.get('table_status'),
+      'status': request.data.get('status'),
+    }
+
+    serializer = TableRestoSerializer(instance=table_resto_instance, data=data, partial=True)
+    if serializer.is_valid():
+      serializer.save()
+      response = {
+        'status': status.HTTP_200_OK,
+        'message': 'Data updated successfully',
+        'data': serializer.data
+      }
+      return Response(response, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+  # method DELETE
+  def delete(self, request, id, *args, **kwargs):
+    table_resto_instance = self.get_object(id)
+    if not table_resto_instance:
+      return Response(
+        {
+          'status': status.HTTP_400_BAD_REQUEST,
+          'message': 'Data not found',
+          'data': {}
+        }, status=status.HTTP_400_BAD_REQUEST
+      )
+    
+    table_resto_instance.delete()
+    response = {
+      'status': status.HTTP_200_OK,
+      'message': 'Data deleted successfully',
+    }
+    return Response(response, status=status.HTTP_200_OK)
